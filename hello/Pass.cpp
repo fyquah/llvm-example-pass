8f38b9250e6662276a0b65d9caa0c22811d5104f
Fu Yong Quah <quah.fy95@gmail.com>
2016-09-10 17:50:28 -0400

    initial commit

---
+++ hello/Pass.cpp
@@ -0,0 +1,24 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+class Hello : public FunctionPass {
+    static char ID;
+    Hello() : FunctionPass(ID) {}
+    bool runOnFunction(Function &F) override {
+        errs() << "Hello: ";
+        errs().write_escaped(F.getName()) << "\n";
+        return false;
+    }
+
+} // Hello
+} // namespace
+
+char Hello::ID = 0;
+static RegisterPass<Hello> X("hello", "Hello World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
